2d777c4ce395a8e3e7a282bcdde511137143de22
Елена Чижеумова <shotik57@gmail.com>
2022-05-18 00:40:14 +1000

    lesson-5

--- lesson-1/index.js
+++ lesson-1/index.js
@@ -1,9 +1,4 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-];
+
 
 const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
 const GET_GOODS_ITEMS = `${BASE_URL}catalogData.json`
@@ -14,64 +9,46 @@ function service(url) {
         .then((res) => res.json())
 }
 
-class GoodsItem {
-    constructor({ product_name, price }) {
-        this.product_name = product_name;
-        this.price = price;
-    }
 
-    render() {
-        return `
-        <div class="goods-item">
-        <h3>${this.product_name}</h3>
-        <p>${this.price}</p>
-        </div>`;
-    }
+function init() {
+    const app = new Vue({
+        el: '#root',
+        data: {
+            items: [],
+            filteredItems: [],
+            search: '',
+            isVisibleCart: false,
+        },
+        methods: {
+            fetchGoods() {
+                service(GET_GOODS_ITEMS).then((data) => {
+                    this.items = data;
+                    this.filteredItems = data;
+                });
+            },
+
+            filterItems() {
+                this.filteredItems = this.items.filter(({ product_name }) => {
+                    return product_name.match(new RegExp(this.search, 'gui'))
+                })
+            },
+
+            setVisionCard() {
+                this.isVisibleCart = !this.isVisibleCart;
+            }
+
+        },
+        computed: {
+            calculatePrice() {
+                return this.filteredItems.reduce((prev, { price }) => {
+                    return prev + price
+                }, 0)
+            }
+        },
+
+        mounted() {
+            this.fetchGoods();
+        }
+    })
 }
-
-class GoodsList {
-    items = [];
-    filteredItems = []
-    fetchGoods() {
-        return service(GET_GOODS_ITEMS).then((data) => {
-            this.items = data;
-            this.filteredItems = data;
-        });
-    }
-
-
-    calculatePrice() {
-        return this.items.reduce((prev, { price }) => {
-            return prev + price
-        }, 0)
-    }
-
-
-    render() {
-        let goodsList = this.filteredItems.map(item => {
-            const goodsItem = new GoodsItem(item);
-            return goodsItem.render()
-        }).join('');
-        document.querySelector('.goods-list').innerHTML = goodsList;
-    }
-}
-
-class BasketGoodsList {
-    items = [];
-    fetchGoods() {
-        service(GET_BASKET_GOODS_ITEMS, (data) => {
-            this.items = data.contents;
-        })
-    }
-}
-
-
-const goodsList = new GoodsList();
-goodsList.fetchGoods().then(() => {
-    goodsList.render();
-});
-
-const basketGoodsList = new BasketGoodsList()
-basketGoodsList.fetchGoods();
-
-
+window.onload = init
